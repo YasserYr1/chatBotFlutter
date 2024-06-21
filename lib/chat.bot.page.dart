51c5e8f8c7c943582f7428a934b14c2d2046339b
YasserYr1 <elyasser880@gmail.com>
2024-06-21 17:17:02 +0100

    f&l

---
+++ lib/chat.bot.page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatBotPage extends StatelessWidget {
+const ChatBotPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
